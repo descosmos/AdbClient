7ffc102b71f6ef9ba193bc574a147cb06298a1e6
liqingfeng <liqingfeng@kingsoft.com>
2024-01-16 11:11:15 +0800

    unittest: add string test

--- unittest/hello_test.cpp
+++ unittest/hello_test.cpp
@@ -1,24 +1,47 @@
 #include <gtest/gtest.h>
-#include <string>
 #include <stdio.h>
-#include "utils.h"
+
+#include <string>
+
 #include "HostCommand.h"
 #include "htime.h"
+#include "utils.h"
 
 using namespace hv;
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 + 6, add_t(7, 6));
+typedef std::vector<std::string> vec_str;
+
+TEST(UtilsTest, StringSplitAssertions) {
+    std::string str1 = "Setence 1\nSetence 2";
+    vec_str res1 = string_split(str1, '\n');
+    ASSERT_EQ(res1.size(), 2);
+    ASSERT_EQ(res1[0], std::string("Setence 1"));
+    ASSERT_EQ(res1[1], std::string("Setence 2"));
+
+    std::string str2 = "Setence 1\nSetence 2\n";
+    vec_str res2 = string_split(str2, '\n');
+    ASSERT_EQ(res2.size(), 3);
+    ASSERT_EQ(res2[0], std::string("Setence 1"));
+    ASSERT_EQ(res2[1], std::string("Setence 2"));
+    ASSERT_EQ(res2[2], std::string(""));
+
+    std::string str3 = "Setence 1\tSetence 2";
+    vec_str res3 = string_split(str3, '\t');
+    ASSERT_EQ(res3.size(), 2);
+    ASSERT_EQ(res3[0], std::string("Setence 1"));
+    ASSERT_EQ(res3[1], std::string("Setence 2"));
+}
+
+TEST(UtilsTest, StringUniqueAssertions) {
+    std::string str1 = "Setence1       Setence2";
+    std::string res1 = unique_character(str1);
+    ASSERT_EQ(res1, std::string("Setence1 Setence2"));
 }
 
 TEST(ConnectTest, BasicAssertions) {
-  HostCommand adbcmd;
-  int connfd = adbcmd.m_tcp_client.createsocket(DEFAULT_ADB_PORT, "127.0.0.1");
-  ASSERT_GE(connfd, 0);
+    HostCommand adbcmd;
+    int connfd = adbcmd.m_tcp_client.createsocket(DEFAULT_ADB_PORT, "127.0.0.1");
+    ASSERT_GE(connfd, 0);
 
-  adbcmd.m_tcp_client.closesocket();
+    adbcmd.m_tcp_client.closesocket();
 }
